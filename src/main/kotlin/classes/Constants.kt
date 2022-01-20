fc98e0728ea4c23ce88f9052dbeb6e1b997e0eed
Tyler23333 <Tyler20130638@163.com>
2022-01-19 21:45:54 -0500

    更新注释

--- src/main/kotlin/classes/Constants.kt
+++ src/main/kotlin/classes/Constants.kt
@@ -1,21 +1,34 @@
 package mirai.guyuemochen.chatbot.classes
 
+/**
+ * 所有常量
+ *
+ * @author 古月漠尘
+ */
 object Constants {
 
-    // 登陆时的输出
-    const val loadMessage = "古月出品必属精品。"
+    const val loadMessage = "古月出品必属精品。" // 插件加载发送词
+    const val disableMessage = "欢迎下次再见" // 插件卸载发送词
+
+    object GroupDataDefault {
+
+        const val randomDelay: Long = 3600000 // 一小时
+        const val randomType = RandomType.answerAfterDelay // 默认为跟在最后
+        const val welcomeText = "欢迎@入群" // 默认欢迎词
+
+    }
 
     object RandomType {
 
-        const val answerAfterDelay = 1
-        const val repeatConstantDelay = 2
+        const val answerAfterDelay = 1 // 跟在最后
+        const val repeatConstantDelay = 2 // 固定时间说话
 
     }
 
     object BotStatus {
 
-        const val on = 1
-        const val off = 2
+        const val on = 1 // 机器人开启状态
+        const val off = 2 // 机器人关闭状态
 
     }
 
