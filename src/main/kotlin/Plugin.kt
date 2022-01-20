23793ddfd10b30eae26c03d9096d7d49b68d4bac
Tyler23333 <Tyler20130638@163.com>
2022-01-19 23:36:13 -0500

    重构中

--- src/main/kotlin/Plugin.kt
+++ src/main/kotlin/Plugin.kt
@@ -8,13 +8,13 @@ import net.mamoe.mirai.console.plugin.version
 import net.mamoe.mirai.event.GlobalEventChannel
 import net.mamoe.mirai.utils.info
 
-import kotlin.io.path.Path
-
 // 导入本地class
 import mirai.guyuemochen.chatbot.classes.Constants
 import mirai.guyuemochen.chatbot.data.BotInfo
+import net.mamoe.mirai.event.events.BotOfflineEvent
 
 import net.mamoe.mirai.event.events.BotOnlineEvent
+import net.mamoe.mirai.event.events.GroupMessageEvent
 
 /**
  * plugin的main文件
@@ -30,9 +30,11 @@ object Plugin : KotlinPlugin(
         author("古月漠尘")
     }
 ) {
-    private var bots: MutableList<BotInfo> = mutableListOf()
-    // 当bot启动时
+    private var botInfoList: MutableList<BotInfo> = mutableListOf() // 所有bot所在的info
+
+    // 插件启动时
     override fun onEnable() {
+        // 后台输出所有信息
         logger.info{ "Plugin:${this.name}, id:${this.id}, version:${this.version} enabled." }
         logger.info{ Constants.loadMessage }
 
@@ -41,9 +43,93 @@ object Plugin : KotlinPlugin(
 
         // 机器人登录事件
         eventChannel.subscribeAlways<BotOnlineEvent> {
-            // 加载json文件
             // 获取数据
-            bots.add(BotInfo(this.bot, dataFolderPath))
+            botInfoList.add(BotInfo(this.bot, dataFolderPath))
         }
+
+        eventChannel.subscribeAlways<BotOfflineEvent> {
+            val botInfo = getBotInfoById(this.bot.id, botInfoList)
+            if (botInfo != null){
+                botInfo.saveData()
+                // 将数据移除
+                botInfoList.remove(botInfo)
+            }
+        }
+
+        eventChannel.subscribeAlways<GroupMessageEvent> {
+            val botInfoIndex = getBotInfoIndexById(this.bot.id, botInfoList)
+            // 更新当前群组task，若为定时发送则无需更新
+            botInfoList[botInfoIndex].refreshTask(this.group)
+        }
+    }
+
+    /**
+     * 插件卸载时
+     */
+    override fun onDisable() {
+        super.onDisable()
+        logger.info{ "Plugin:${this.name}, id:${this.id}, version:${this.version} disabled." }
+        logger.info{ Constants.disableMessage }
+
+        for (bot in botInfoList){
+            bot.saveData()
+        }
+    }
+
+    /**
+     * 根据bot的qq号获取信息,运用二叉树寻找法，将复杂度变为log(n)
+     *
+     * @param botId 寻找的bot的qq号
+     * @param botInfoList 所有bot的信息
+     *
+     * @return 返回对应的bot，若不存在则返回null
+     */
+    private fun getBotInfoById(botId : Long, botInfoList: MutableList<BotInfo>): BotInfo?{
+
+        if (botInfoList.size == 1){
+            if(botInfoList[0].botId == botId){
+                return botInfoList[0]
+            }
+        }
+        else if (botInfoList.size > 1){
+            val left = getBotInfoById(botId, botInfoList.subList(0, botInfoList.size/2))
+            if (left != null){
+                return left
+            }
+
+            val right = getBotInfoById(botId, botInfoList.subList(botInfoList.size/2, botInfoList.size))
+            if(right != null){
+                return right
+            }
+        }
+        return null
+    }
+
+    /**
+     * 根据bot的qq号获取信息,运用二叉树寻找法，将复杂度变为log(n)
+     *
+     * @param botId 寻找的bot的qq号
+     * @param botInfoList 所有bot的信息
+     *
+     * @return 返回对应的bot，若不存在则返回null
+     */
+    private fun getBotInfoIndexById(botId : Long, botInfoList: MutableList<BotInfo>): Int{
+        if (botInfoList.size == 1){
+            if(botInfoList[0].botId == botId){
+                return 0
+            }
+        }
+        else if (botInfoList.size > 1){
+            val leftIndex = getBotInfoIndexById(botId, botInfoList.subList(0, botInfoList.size/2))
+            if (leftIndex != -1){
+                return leftIndex
+            }
+
+            val rightIndex = getBotInfoIndexById(botId, botInfoList.subList(botInfoList.size/2, botInfoList.size))
+            if(rightIndex != -1){
+                return rightIndex + botInfoList.size/2
+            }
+        }
+        return -1
     }
 }
